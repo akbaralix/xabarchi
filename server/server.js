9b28bbc3922cc13509073e43a23bc060761bb5de
Your Name <you@example.com>
2026-03-10 23:11:26 +0500

    17-m

--- server/server.js
+++ server/server.js
@@ -207,7 +207,7 @@ app.use(postRouter);
 app.use(chatRouter);
 
 app.use(express.static(path.join(__dirname, "..", "dist")));
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
 });
 
